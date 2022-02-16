3b038d7c38e31301d88ba881fb6d7bf5de99cb1f
Rene Mailaender <rene.mailaender@homevoice.io>
2022-02-17 00:07:20 +0100

    change naming

--- src/Result.ts
+++ src/Result.ts
@@ -1,58 +1,58 @@
 // -----------------------------------------------------------------------------
 // model
 // -----------------------------------------------------------------------------
-export type Err<E, T> =
-  InstanceOperations<E, T> &
-  Deconstructos<E, T> &
+export type Err<ErrType, OkType> =
+  ResultOperations<ErrType, OkType> &
+  ResultDeconstructors<ErrType, OkType> &
   {
     readonly _tag: 'err';
-    readonly err: E;
+    readonly err: ErrType;
   };
 
-export type Ok<E, T> =
-  InstanceOperations<E, T> &
-  Deconstructos<E, T> &
+export type Ok<ErrType, OkType> =
+  ResultOperations<ErrType, OkType> &
+  ResultDeconstructors<ErrType, OkType> &
   {
     readonly _tag: 'ok';
-    readonly value: T;
+    readonly value: OkType;
   };
 
-export type Result<E, T> = Err<E, T> | Ok<E, T>;
+export type Result<ErrType, OkType> = Err<ErrType, OkType> | Ok<ErrType, OkType>;
 
 // -----------------------------------------------------------------------------
 // interfaces
 // -----------------------------------------------------------------------------
 
-type InstanceOperations<E, A> = {
-  map: <B>(fn: (value: A) => B) => Result<E, B>;
-  mapErr: <B>(fn: (err: E) => B) => Result<B, A>;
-  andThen: <B, EB>(fn: (a: A) => Result<EB, B>) => Result<E | EB, B>;
+type ResultOperations<ErrType, OkType> = {
+  map: <OkReturn>(onOk: (value: OkType) => OkReturn) => Result<ErrType, OkReturn>;
+  mapErr: <ErrReturn>(onErr: (err: ErrType) => ErrReturn) => Result<ErrReturn, OkType>;
+  andThen: <ErrReturn, OkReturn>(fn: (value: OkType) => Result<ErrReturn, OkReturn>) => Result<ErrType | ErrReturn, OkReturn>;
 }
 
-type Deconstructos<E, A> = {
-  getOrElse:  <B>(fn: (err: E) => B) => A | B;
-  match: <B, C>(onErr: (err: E) => C, onOk: (value: A) => B) => B | C
+type ResultDeconstructors<ErrType, OkType> = {
+  withDefault:  <DefaultType>(onErr: (err: ErrType) => DefaultType) => OkType | DefaultType;
+  match: <ErrReturn, OkReturn>(onErr: (err: ErrType) => ErrReturn, onOk: (value: OkType) => OkReturn) => OkReturn | ErrReturn
 }
 
 // -----------------------------------------------------------------------------
 // constructors
 // -----------------------------------------------------------------------------
-export const err = <E = never, T = never>(e: E): Result<E, T> => ({
+export const err = <ErrType = never, OkType = never>(e: ErrType): Result<ErrType, OkType> => ({
   _tag: 'err',
   err: e,
   map: () => err(e),
-  mapErr: fn => err(fn(e)),
+  mapErr: onErr => err(onErr(e)),
   andThen: () => err(e),
-  getOrElse: fn => fn(e),
+  withDefault: onErr => onErr(e),
   match: (onError) => onError(e)
 });
 
-export const ok = <E = never, T = never>(value: T): Result<E, T> => ({
+export const ok = <ErrType = never, OkType = never>(value: OkType): Result<ErrType, OkType> => ({
   _tag: 'ok',
   value,
-  map: fn => ok(fn(value)),
+  map: onOk => ok(onOk(value)),
   mapErr: () => ok(value),
-  andThen: fn => fn(value),
-  getOrElse: () => value,
+  andThen: onOk => onOk(value),
+  withDefault: () => value,
   match: (_, onOk) => onOk(value)
 });
